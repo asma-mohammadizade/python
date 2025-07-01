db9add714f1862200d0ccfc846266d2b54d38ae8
Asma Mohammadizade <asma.mohammadi24@gmail.com>
2025-07-01 19:53:14 +0330

    edit Day08

--- Day08/8.py
+++ Day08/8.py
@@ -59,39 +59,6 @@ if direction == "encode":
 elif direction == "decode":
     decrypt(cipher_text=text, shift_amount=shift)
 
-## way 2
 
-# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# direction = input("type 'encode' to encrypt type 'decode' to decrypt:\n")
-# text = input ("type your message:\n").lower()
-# shift = int(input("type the shift number:\n"))
-
-# def caesar(start_text, shift_amount, cipher_direction):
-#     end_text = ""
-#     if cipher_direction == "decode":
-#         shift_amount *= -1
-#     for letter in start_text:
-#         if letter in alphabet:
-#             position = alphabet.index(letter)
-#             new_position = position + shift_amount
-#             end_text += alphabet[new_position]
-#         else:
-#             end_text += letter
-
-#     print(f"the {cipher_direction}d text is {end_text}")
-
-
-# should_continue = True
-# while should_continue:
-#     direction = input("type 'encode' to encrypt type 'decode' to decrypt:\n")
-#     text = input ("type your message:\n").lower()
-#     shift = int(input("type the shift number:\n"))
-
-#     shift = shift % 26
-#     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-#     result = input("type 'yes' if you want to go again. otherwise type 'no'.\n")
-#     if result == "no":
-#          should_continue = False
-#          print("good bye")
 
 
